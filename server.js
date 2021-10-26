b08b2874451736617f3c7c1254c62887eeb15aed
fab1214 <88295561+fab1214@users.noreply.github.com>
2021-10-26 18:26:30 -0400

    add feature to add animals with unique ID, validate user input

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming POST data and convert to key/value pair; 
+//extended:true says to look as deep as possible for nested sub-arrays
+app.use(express.urlencoded({ extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 //require the data for route creation for front-end data request
 const  { animals } = require('./data/animals');
 
@@ -48,6 +55,22 @@ function findById(id, animalsArray) {
   return result;
 }
 
+//take data from req.body and add to animalsArray
+function createNewAnimal(body, animalsArray){
+  const animal = body;
+  //add created animal to animalsArry
+  animalsArray.push(animal);
+  //write in new animal data into json file
+  fs.writeFileSync(
+    //write json file in data subdirectory and join with directory of the file we executed originally
+    path.join(__dirname, './data/animals.json'),
+    //convert data to json; null = dont edit existing data, 2 = create white space between values
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
 //add the route to get animal results
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -67,6 +90,43 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+//allow users to post data to server from client
+app.post('/api/animals', (req, res) => {
+  // add "id" to body and set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  //run validateAnimal and pass in req.body info as parameter
+  //if any data in req.body is incorrect, send error 404 back
+  if(!validateAnimal(req.body)){
+    res.status(404).send('The animal is not properly formatted.')
+  }else{
+  //add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
+  }
+});
+
+//validate info for adding animals by adding const animal as parameter
+function validateAnimal(animal) {
+  //if animal.name doesnt exist, or is not a string, return false
+  if(!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  //if animal.species doesnt exist, or is not a string, return false
+  if(!animal.species || typeof animal.species !== 'string'){
+    return false;
+  }
+  //if animal.diet doesnt exist, or is not a string, return false
+  if(!animal.diet || typeof animal.diet !== 'string'){
+    return false;
+  }
+  //if animal.personalityTraits doesnt exist, or is not an array, return false
+  if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+    return false;
+  }
+  //if any of these are strings, return true and submit info
+  return true;
+}
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
